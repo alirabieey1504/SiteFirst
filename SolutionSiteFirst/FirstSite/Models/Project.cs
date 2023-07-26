f22ab7556562e2cf2f7f7693b9d0d998b541ac64
alirabieey1504 <alidevops1504@gmail.com>
2023-07-26 18:31:53 +0430

    component view moved page first

--- SolutionSiteFirst/FirstSite/Models/Project.cs
+++ SolutionSiteFirst/FirstSite/Models/Project.cs
@@ -9,6 +9,16 @@ namespace FirstSite.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description{ get; set; }
-        public string  client { get; set; }
+        public string  Client { get; set; }
+        public string Img { get; set; }
+
+        public Project(long id, string name, string description, string client,string img)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Client = client;
+            Img = img;
+        }
     }
 }
---
+++ SolutionSiteFirst/FirstSite/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using FirstSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstSite.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Project=new List<Project>
+            {
+                new Project(id:1,name:"پروژه دی جی کالا",description:"سفارش کالا  با بهترین کیفیت در کمترین زمان",client:"digikala",img:"project-7.jpg"),
+                new Project(id:2,name:"شاپ مارکت",description:"درخواست میوه و سبزیجات در کمترین زمان ",client:"shapmarket",img:"project-1.jpg"),
+                new Project(id:3,name:"mobilenet",description:"سفارش کالا  با بهترین کیفیت در کمترین زمان",client:"mobilenet  ",img:"project-2.jpg"),
+                new Project(id:4,name:"berandsho",description:"سفارش کالا  با بهترین کیفیت در کمترین زمان",client:"berandsho  ",img:"project-3.jpg"),
+            };
+            return View("_Projects",Project);
+        }
+    }
+}
