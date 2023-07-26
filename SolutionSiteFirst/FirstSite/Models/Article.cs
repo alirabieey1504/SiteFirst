36cebd57db38cb8771e3158c65d557eb078aa953
alirabieey1504 <alidevops1504@gmail.com>
2023-07-26 18:54:54 +0430

    footer moved page to component view

--- SolutionSiteFirst/FirstSite/Models/Article.cs
+++ SolutionSiteFirst/FirstSite/Models/Article.cs
@@ -5,5 +5,15 @@
         public long Id { get; set; }
         public string Title  { get; set; }
         public string Description { get; set; }
+        public string Img { get; set; }
+
+        public Article(long id, string title, string description, string img)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Img = img;
+        }
     }
+
 }
---
+++ SolutionSiteFirst/FirstSite/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstSite.ViewComponents
+{
+    public class ArticlesViewComponent :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Article=new List<Article>
+            {
+                new Article(id:1,title:"magale1",description:"اولین پروژه های سایت ما",img:"blog-post-thumb-6.jpg"),
+                new Article(id:2,title:"magale2",description:"دومین پروژه های سایت ما",img:"blog-post-thumb-3.jpg"),
+                new Article(id:3,title:"magale3",description:"سومین پروژه های سایت ما",img:"blog-post-thumb-4.jpg"),
+                new Article(id:4,title:"magale4",description:"چهارمین پروژه های سایت ما",img:"blog-post-thumb-5.jpg"),
+            };
+            return View("_Articles",Article);
+        }
+    }
+}
