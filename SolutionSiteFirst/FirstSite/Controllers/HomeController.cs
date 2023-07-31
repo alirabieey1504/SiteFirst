ee030dc11d9c7155c770cf5d041299e5d697064f
alirabieey1504 <alidevops1504@gmail.com>
2023-07-31 16:45:36 +0430

    moved page blog and contact to progect

--- SolutionSiteFirst/FirstSite/Controllers/HomeController.cs
+++ SolutionSiteFirst/FirstSite/Controllers/HomeController.cs
@@ -16,6 +16,28 @@ namespace FirstSite.Controllers
         {
             
         }
+        [HttpGet]
+        public IActionResult formexe()
+        {
+            var key=new FormKeyModel();
+            return View(key);
+        }
+
+        [HttpPost]
+        public IActionResult formexe(FormKeyModel model)
+        {
+            if (ModelState.IsValid==false)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نیست لطفا دوباره تلاش کنید";
+                return View(model);
+            }
+            else
+            {
+                ViewBag.success = "با موفقیت ثبت شد";
+                return View();
+            }
+            
+        }
 
         public IActionResult Index()
         {
@@ -31,19 +53,37 @@ namespace FirstSite.Controllers
         }
 
       //  [HttpPost]
-        //public JsonResult Contact(IFormCollection form)
+        //public JsonResult Contact(IFormCollection model)
         //{
-        //    var email = form["email"];
+        //    var email = model["email"];
 
         //    return Json(Ok());
         //}
 
 
+        //[HttpPost]
+        //public JsonResult Contact(ContactModel model)
+        //{
+        //    Console.WriteLine(model.ToString());
+        //    return Json(Ok());
+        //}
+
+
         [HttpPost]
-        public JsonResult Contact(ContactModel form)
+        public IActionResult Contact(ContactModel model)
         {
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
+            if (ModelState.IsValid == false)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نیست.لطفا دوباره تلاش کنید.";
+                return View(model);
+
+            }
+            else
+            {
+                ViewBag.success = "اطلاعات شما با موفقیت ثبت شد";
+                return View();
+                //   return RedirectToAction("Index");
+            }
         }
       
 
---
+++ SolutionSiteFirst/FirstSite/Models/FormKeyModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstSite.Models
+{
+    public class FormKeyModel
+    {
+        [Required(ErrorMessage = "لطفا نام را تصحیح وارد کنید")]
+        [MaxLength(10,ErrorMessage = "بیش از حد کاراکتر وارد کردی !!")]
+        [MinLength(3,ErrorMessage = "حداقل ورودی کاراکتر 3 میباشد")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "لطفا فامیلی را تصحیح وارد کنید")]
+        [MaxLength(10, ErrorMessage = "بیش از حد کاراکتر وارد کردی !!")]
+        [MinLength(3, ErrorMessage = "حداقل ورودی کاراکتر 3 میباشد")]
+        public string Family { get; set; }
+        [Required(ErrorMessage = "لطفاکد ملی را صحیح وارد کنید")]
+        
+        public long Kodmeli { get; set; }
+        [Required(ErrorMessage = "لطفا ایمیل  را تصحیح وارد کنید")]
+        [MaxLength(40, ErrorMessage = "بیش از حد کاراکتر وارد کردی !!")]
+        [MinLength(3, ErrorMessage = "حداقل ورودی کاراکتر 3 میباشد")]
+        public string Email { get; set; }
+    }
+}
